7d27002b24eb46ad865b539d150f7e2874ed1728
Kai Peterson <pete9372@umn.edu>
2019-10-02 08:58:34 -0500

    solve and test base problems

--- client.js
+++ client.js
@@ -41,3 +41,54 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+for (i = 0; i < employees.length; i++) {
+  console.log(employeeBonus(employees[i]));
+}
+
+function employeeBonus(employee) {
+  let newEmployeeObject = {};
+  let bonusPercentage = bonusCalculation(employee);
+  let totalBonus = Math.round(employee.annualSalary * bonusPercentage);
+  let totalCompensation = totalBonus + Number(employee.annualSalary);
+  
+  newEmployeeObject.name = employee.name;
+  newEmployeeObject.bonusPercentage = bonusPercentage;
+  newEmployeeObject.totalBonus = totalBonus;
+  newEmployeeObject.totalCompensation = totalCompensation;
+  return newEmployeeObject;
+  
+}
+
+function bonusCalculation(employee) {
+  let bonusPercentage = 0;
+  if (employee.reviewRating === 3) {
+    // get their income
+    // calculate 4% of their income
+    // add bonus to the variable
+    bonusPercentage += 0.04;
+  }
+  else if (employee.reviewRating === 4) {
+    // calc 6% of income
+    // add bonus to the variable
+    bonusPercentage += 0.06;
+  }
+  else if (employee.reviewRating === 5) {
+    // 10% of income
+    // add bonus to the variable
+    bonusPercentage += 0.10;
+  }
+  if (employee.employeeNumber.length === 4) {
+    bonusPercentage += 0.05;
+  }
+  if (employee.annualSalary > 65000) {
+    bonusPercentage -= .01;
+  }
+  if (bonusPercentage > 0.13) {
+    bonusPercentage = 0.13;
+  }
+  if (bonusPercentage < 0) {
+    bonusPercentage = 0;
+  }
+  return bonusPercentage;
+}
